e1e54eb86fa0e264d22a7cd08ee4f2d0ad1d21c7
Gantsogte <gancogte@gmail.com>
2017-04-25 11:55:16 +0900

    Update Game.cs
    LevelOne-iin mapaas anhnii bairshiluudaa unshin objectuudaa uusgene, negees olon flag, box uusgeh asuudal !!!

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,40 @@ namespace Sokoban
 {
     class Game
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //LEFT = 1, UP = 2, RIGHT = 3, DOWN = 4;
+            Level lvl = new Level();
+            lvl.DrawMap();
+            
+            //negees olon flag, boxiin object uusgeh !!!!
+            Flag[4] flag;
+            Box[4] box;
 
+            int i, j;
+            //mapaa unshij objectuudaa baiguulna
+            for(i=0; i<8; i++)
+            {
+                for(j=0; j<8; j++)
+                {
+                    int mapNodeValue = lvl.GetMaps(i, j);
+                    if(mapNodeValue == 1)
+                    {
+                        Flag flag = new Flag(i, j);
+                    }
+                    if(mapNodeValue == 3)
+                    {
+                        Box box = new Box(i, j);
+                    }
+                    if(mapNodeValue == 5)
+                    {
+                        BoxMan boxMan = new BoxMan(i, j);
+                    }
+                }
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
