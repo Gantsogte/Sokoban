6f070541b6287564ab6deb29e0f396bfdad54a76
Gantsogte <gancogte@gmail.com>
2017-04-27 23:23:21 +0900

    Update Flag.cs

--- Flag.cs
+++ Flag.cs
@@ -11,18 +11,28 @@ namespace Sokoban
         /*Hairtsagiig avaachih etssiin bairshil
          Console deer flagiig 1 eer zurna*/
         int posFlagX, posFlagY;
-        const int FLAGVALUE = 1;
+        public static int FLAG = 1;// 1 is value of flag also means FLAG
         public Flag(int x, int y)
         {
             this.posFlagX = x;
             this.posFlagY = y;
         }
-
-        //flag zurah
-        public void DrawFlag(int x, int y)
+        // x, y positionii utgiig uguh
+        public void SetFlagPos(int x, int y)
         {
-            Console.SetCursorPosition(x, y);
-            Console.Write('1');
+            this.posFlagX = x;
+            this.posFlagY = y;
+        }
+        // x positionii utgiig avah
+        public int GetFlagPosX()
+        {
+            return this.posFlagX;
+        }
+        // y positionii utgiig avah
+        public int GetFlagPosY()
+        {
+            return this.posFlagY;
         }
     }
 }
+
