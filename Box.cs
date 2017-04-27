976e37dcb7deed3133d6eb48f188fe4611c51ba8
Gantsogte <gancogte@gmail.com>
2017-04-27 23:22:11 +0900

    Update Box.cs
    Зурах хэсэг нь тусдаа өөр зурдаг классд орно.

--- Box.cs
+++ Box.cs
@@ -11,18 +11,48 @@ namespace Sokoban
         /*Hairtsag, mass ni 3
          Console deer 3 gej zuragdana*/
         int posBoxX, posBoxY;
-        const int BOXMASS = 3;
+        const int LEFT = 1, UP = 2, RIGHT = 3, DOWN = 4;
+        public static int BOX = 3;// 3 is mass of box also means BOX
+        //baiguulagch
         public Box(int x, int y)
         {
             this.posBoxX = x;
             this.posBoxY = y;
         }
-
-        //box zurah
-        public void DrawBox(int x, int y)
+        // x, y positionii utgiig uguh
+        public void SetBoxPos(int x, int y)
         {
-            Console.SetCursorPosition(x, y);
-            Console.Write('3');
+            this.posBoxX = x;
+            this.posBoxY = y;
+        }
+        // x positionii utgiig avah
+        public int GetBoxPosX()
+        {
+            return this.posBoxX;
+        }
+        // y positionii utgiig avah
+        public int GetBoxPosY()
+        {
+            return this.posBoxY;
+        }
+
+        public void MoveBox(int direction)
+        {
+            switch (direction)
+            {
+                case LEFT:
+                    this.posBoxX--;
+                    break;
+                case UP:
+                    this.posBoxY--;
+                    break;
+                case RIGHT:
+                    this.posBoxX++;
+                    break;
+                case DOWN:
+                    this.posBoxY++;
+                    break;
+            }
         }
     }
 }
