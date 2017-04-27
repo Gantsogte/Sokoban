229cd330896e65ce63057350fbbda3c96814860b
Gantsogte <gancogte@gmail.com>
2017-04-27 23:25:36 +0900

    Update Level.cs

--- Level.cs
+++ Level.cs
@@ -8,52 +8,39 @@ namespace Sokoban
 {
     class Level
     {
-        public static int[,] map = new int[8, 8];
+        public static int HEIGHT = 8, WIDTH = 8;
+        public const int WALL = 9, EMPTY = 0;
+        public int[,] map = new int[HEIGHT, WIDTH];
         public Level()
         {
             LevelOne();
         }
 
-        //level 1-iin baiguulagch
+        //level 1-iin map-d utga olgoh
         private void LevelOne()
         {
-            int i;
-            for(i=0; i<8; i++)
+            int column;
+            for(column=0; column<HEIGHT; column++)
             {
-                if (i == 1) { map[i, 1] = 9; map[i, 2] = 9; map[i, 3] = 9; map[i, 4] = 1; map[i, 5] = 9; map[i, 6] = 9; }
-                if (i == 2) { map[i, 1] = 9; map[i, 2] = 9; map[i, 3] = 9; map[i, 4] = 0; map[i, 5] = 9; map[i, 6] = 9; }
-                if (i == 3) { map[i, 1] = 1; map[i, 2] = 0; map[i, 3] = 3; map[i, 4] = 3; map[i, 5] = 9; map[i, 6] = 9; }
-                if (i == 4) { map[i, 1] = 9; map[i, 2] = 9; map[i, 3] = 5; map[i, 4] = 3; map[i, 5] = 0; map[i, 6] = 1; }
-                if (i == 5) { map[i, 1] = 9; map[i, 2] = 9; map[i, 3] = 3; map[i, 4] = 9; map[i, 5] = 9; map[i, 6] = 9; }
-                if (i == 6) { map[i, 1] = 9; map[i, 2] = 9; map[i, 3] = 1; map[i, 4] = 9; map[i, 5] = 9; map[i, 6] = 9; }
-                map[0, i] = 9;
-                map[i, 0] = 9;
-                map[7, i] = 9;
-                map[i, 7] = 9;
+                //gadna hureend utga uguh
+                map[0, column] = WALL;
+                map[column, 0] = WALL;
+                map[7, column] = WALL;
+                map[column, 7] = WALL;
+                //Dotorh utguudiig uguh
+                if (column == 1) { map[column, 1] = WALL;      map[column, 2] = WALL;  map[column, 3] = WALL;          map[column, 4] = Flag.FLAG; map[column, 5] = WALL;  map[column, 6] = WALL; }
+                if (column == 2) { map[column, 1] = WALL;      map[column, 2] = WALL;  map[column, 3] = WALL;          map[column, 4] = EMPTY;     map[column, 5] = WALL;  map[column, 6] = WALL; }
+                if (column == 3) { map[column, 1] = Flag.FLAG; map[column, 2] = EMPTY; map[column, 3] = Box.BOX;       map[column, 4] = Box.BOX;   map[column, 5] = WALL;  map[column, 6] = WALL; }
+                if (column == 4) { map[column, 1] = WALL;      map[column, 2] = WALL;  map[column, 3] = BoxMan.BOXMAN; map[column, 4] = Box.BOX;   map[column, 5] = EMPTY; map[column, 6] = Flag.FLAG; }
+                if (column == 5) { map[column, 1] = WALL;      map[column, 2] = WALL;  map[column, 3] = Box.BOX;       map[column, 4] = WALL;      map[column, 5] = WALL;  map[column, 6] = WALL; }
+                if (column == 6) { map[column, 1] = WALL;      map[column, 2] = WALL;  map[column, 3] = Flag.FLAG;     map[column, 4] = WALL;      map[column, 5] = WALL;  map[column, 6] = WALL; }
             }
-            //Console.WriteLine("Map Loaded\n");
-            //throw new NotImplementedException();
         }
 
         //mapiing neg nudiing utga avah
-        public int GetMaps(int x, int y)
+        public int GetMapNodeValue(int posX, int poxY)
         {
-            return this.map[x,y];
-        }
-
-        //map zurah
-        public void DrawMap()
-        {
-            int i, j;
-            for(i=0; i<8; i++)
-            {
-                for(j=7; j>=0; j--)
-                {
-                    Console.SetCursorPosition(j, i);
-                    Console.Write(map[i, j]);
-                }
-            }
+            return this.map[posX, poxY];
         }
     }
-    
 }
